b9237a57bd83e9ee9b50b5abf1a926d3b73202d4
Richard Matheus Flôr Dorotéio - 823131674 <823131674@ulife.com.br>
2024-08-15 21:34:01 -0300

    Aula 15/08

---
+++ Contador.java
@@ -0,0 +1,12 @@
+public class Contador{
+
+    public static void main(String[] args){
+       
+        int contador = 0;
+       for (int i = 0; i <= 10; i++){
+        System.out.println("Contando " + i);
+       contador +=1;
+            }
+            System.out.println("Quantidade = " + contador);
+        }
+    }
---
+++ Vetor/VetoresSimples.java
@@ -0,0 +1,23 @@
+package Vetor;
+
+public class VetoresSimples {
+    public static void main(String[] args ){
+        
+    //Declarando vetores
+       int [] vetorDeInteiros;
+       String [] vetorDeStrings;
+       double [] vetorDeDouble;
+       boolean [] vetorDeBooleans;
+
+    //Construindo vetores indiretamente
+    vetorDeInteiros = new int[10];
+
+    //Construindo vetores diretamente
+    int [] vetorDeInteiros2 = new int[10];
+
+    int tamanho = vetorDeInteiros.length;
+    System.out.println("O tamano do vetor é: " + tamanho);
+
+    }
+    
+}
