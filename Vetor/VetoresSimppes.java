05dd7f322d184b75d18593a0cf60a540fce709f9
Danilo-Miguel <prof.danilomiguel@ulife.com.br>
2024-08-21 18:52:43 -0300

    Primeira aula - 21/08/2024

---
+++ Vetor/VetoresSimppes.java
@@ -0,0 +1,34 @@
+package Vetor;
+public class VetoresSimppes {
+    public static void main(String[] args) {
+
+ // Declarando  vetores
+   int [] vetorDeInteiros;
+   String [] vetorDeStrings;
+    double [] vetorDeDoubles;
+    boolean [] vetorDeBooleans;
+
+    // Construindo vetores indiretamente
+    vetorDeInteiros = new int[10];
+    // Construindo vetores diretamente
+    
+int tamanho  = vetorDeInteiros.length;
+System.out.println(tamanho);
+
+vetorDeInteiros[0] = 10;
+
+
+    // System.out.println(" O indice 0 representa o vetor" + vetorDeInteiros[1]);
+    // for( int a = 0; ; a++){
+    //     System.out.println("O indice " + a + " representa o vetor" + vetorDeInteiros[a]);
+
+    // }
+    //     System.out.println("O indice " + a + " representa o vetor" + vetorDeInteiros[a]);
+    // }
+// int tamanho = vetorDeInteiros.length;
+// System.out.println("O tamanho do vetor é: " + tamanho);
+}
+ 
+    }
+    
+
